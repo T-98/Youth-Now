c316155bc3a3489665ab1fb107f30f230f2befb2
T-98 <37400401+T-98@users.noreply.github.com>
2019-05-09 14:18:14 -0700

    TestingDatabse.py

---
+++ TestingDatabse.py
@@ -0,0 +1,6 @@
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
+db=firebase.database()
+result = firebase.get('/users', None)
+print result
+{'1': 'John Doe', '2': 'Jane Doe'}
