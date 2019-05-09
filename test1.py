83a79747d8382f33539eea2ceebc9d202ebc3ffc
T-98 <37400401+T-98@users.noreply.github.com>
2019-05-09 14:26:57 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,8 +1,9 @@
 import pandas as pd 
 df=pd.read_csv("try.csv")
-# print df["Name:"]
+print df["Name:"]
 for i in range(970):
 	print df["Name:"][i] 
 	print "---"
+	#Connection with API and conversion into JSON
 	data = {"Name": df["Name:"][i]}
 	db.child("users").push(data)
