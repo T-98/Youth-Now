469d937db528d62963b26afe3be436224064fb14
T-98 <37400401+T-98@users.noreply.github.com>
2019-05-29 19:22:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Basically we're requiring our Express functionality, then attaching a "router" variable to Express's router method, then using that method when an attempt is made to HTTP get the top level directory of our website. Finally we export our router function back to our app.
 var express = require('express');
 var router = express.Router();
 
